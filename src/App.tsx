7c022f7c714e68943156257ed68adacf21560d4f
João Victor <joaovictor.ribeiro1300@gmail.com>
2023-08-24 20:35:39 -0300

    feat: remove useContext

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,9 @@ import {
   getAuthorizationToken,
   verifyLoggedIn,
 } from './shared/functions/connection/auth';
-import { useGlobalContext } from './shared/hooks/useGlobalContext';
 import { useNotification } from './shared/hooks/useNotification';
 import { useRequests } from './shared/hooks/useRequests';
+import { useGlobalReducer } from './store/reducers/globalReducer/useGlobalReducer';
 
 const routes: RouteObject[] = [...loginRoutes];
 const routerLoggedIn: RouteObject[] = [
@@ -34,7 +34,7 @@ const router: RemixRouter = createBrowserRouter([...routes, ...routerLoggedIn]);
 
 function App() {
   const { contextHolder } = useNotification();
-  const { setUser } = useGlobalContext();
+  const { setUser } = useGlobalReducer();
   const { request } = useRequests();
 
   useEffect(() => {
